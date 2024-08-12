341efd261b933b3c16dadb6484870519978c42b5
riddhi_gohil <gohilriddhi21@gmail.com>
2024-08-11 23:08:24 -0700

    changed code

--- pull-upstream/entrypoint.py
+++ pull-upstream/entrypoint.py
@@ -10,17 +10,14 @@ def run_command(command, cwd=None):
         print(f"Error occurred while running command: {e}")
         print(f"Error Output: {e.stderr}")
 
-def pull_from_bitbucket_to_github(bitbucket_repo_url, github_repo_path):
+def pull_from_bitbucket_to_github(bitbucket_repo_url):
     """Pull changes from Bitbucket and push to GitHub."""
-    os.chdir(github_repo_path)
-    run_command(f'git remote add bitbucket {bitbucket_repo_url}', cwd=github_repo_path)
-    run_command('git fetch bitbucket', cwd=github_repo_path)
+    run_command(f'git remote add bitbucket {bitbucket_repo_url}')
+    run_command('git fetch bitbucket')
 
 def main():
     BITBUCKET_REPO = os.environ.get('BITBUCKET_REPO')
-    REPO_GITHUB = os.environ.get('REPO_GITHUB')
-    branch = 'main' 
-    pull_from_bitbucket_to_github(BITBUCKET_REPO, REPO_GITHUB, branch)
+    pull_from_bitbucket_to_github(BITBUCKET_REPO)
 
 if __name__ == '__main__':
     main()
