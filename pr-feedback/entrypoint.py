a3ce9c6c9e2d050fce3929c2e2c7a681e7e99855
riddhi_gohil <gohilriddhi21@gmail.com>
2024-09-07 19:19:07 -0700

    testing

--- pr-feedback/entrypoint.py
+++ pr-feedback/entrypoint.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 import logging
 
@@ -17,16 +18,18 @@ def run_command(command, check=True):
   stdout, stderr = process.communicate()
   logger.debug(f"stdout: {stdout}")
   if check and process.returncode != 0:
-    logger.error(f"Command failed with exit code {process.returncode}")
-    logger.error(f"Command stderr: {stderr}")
-    raise subprocess.CalledProcessError(process.returncode, command, stdout, stderr)
+      logger.error(f"Command failed with exit code {process.returncode}")
+      logger.error(f"Command stderr: {stderr}")
+      raise subprocess.CalledProcessError(process.returncode, command, stdout, stderr)
   return stdout.strip()
 
 
-def get_pull_request_diff(pr_number):
+def get_pull_request_diff():
   """Fetches the Git diff for a given pull request number."""
-  command = "git branch"
-  result = run_command(command, check=False)
+  current_branch = run_command("git branch --show-current")
+  print("\nCurrent Branch: ", current_branch)
+  command = "git diff main"
+  result = run_command(command)
   return result
 
 
@@ -38,14 +41,21 @@ def send_diff_to_llm(diff):
 
 
 def main():
+  try:
+    run_command("git config --global --add safe.directory /github/workspace")
+    
     # PR_TITLE = os.getenv("PR_TITLE")
-    ISSUE_NUMBER = os.getenv("ISSUE_NUMBER")
-    pr_number = ISSUE_NUMBER  
-    diff = get_pull_request_diff(pr_number)
-    print("Diff"*10)
+    # ISSUE_NUMBER = os.getenv("ISSUE_NUMBER")
+    
+    diff = get_pull_request_diff()
+    print("\nDiff"*10)
     print(diff)
+    
     feedback = send_diff_to_llm(diff)
     print(feedback)
+  except Exception as e:
+    logger.error(f"An error occurred: {str(e)}")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
