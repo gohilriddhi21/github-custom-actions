dd75a84d37c70d907c9be61c7faa8ad08c26067c
riddhi_gohil <gohilriddhi21@gmail.com>
2024-09-08 23:46:42 -0700

    command git diff updated

--- pr-feedback/entrypoint.py
+++ pr-feedback/entrypoint.py
@@ -31,7 +31,7 @@ def get_pull_request_diff():
   """Fetches the Git diff for a given pull request number."""
   current_branch = run_command("git branch --show-current")
   print("\nCurrent Branch: ", current_branch)
-  command = "git diff main"
+  command = "git diff"
   result = run_command(command)
   return result
 
